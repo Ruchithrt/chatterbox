57b915787f82c135001797e664a3d2aafda71b75
Ruchithrt <ruchithtilavalli3266@gmail.com>
2024-02-11 16:01:29 +0530

    mess, convo , sidebar

--- server/middleware/protectRoute.js
+++ server/middleware/protectRoute.js
@@ -3,23 +3,32 @@ import User from "../model/userModel.js";
 
 const protectRoute = async (req, res, next) => {
   try {
-
     //requestin jwt from cookie
-    const token = req.cookie.jwt;
-    if(!token){
-       return  res.status(401).json({error:"Unathourised - No Token Provided"})
+    const token = req.cookies.jwt;
+    if (!token) {
+      return res
+        .status(401)
+        .json({ error: "Unathourised - No Token Provided" });
     }
 
     //decode the logic using secret key
-    const decode = jwt.verify(token,"N73BF-DBQWHDGQWJWQ1,SDJFSD`~")
-    if(!decode){
-        return  res.status(401).json({error:"Unathourised - Invalid token"})
+    const decode = jwt.verify(token, "N73BF-DBQWHDGQWJWQ1,SDJFSD`~");
+    if (!decode) {
+      return res.status(401).json({ error: "Unathourised - Invalid token" });
     }
 
-    const user = await User.findById(decode.userId).select("-password")
+    const user = await User.findById(decode.userId).select("-password");
 
+    if (!user) {
+      return res.status(400).json({ error: "User not found" });
+    }
+
+    req.user = user;
+    next();
   } catch (error) {
-    console.log("Error in logout controller : ", error.message);
+    console.log("Error in protectRoute controller : ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export default protectRoute;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js"; // adding .js is must when using import and not require
 import messageRoutes from "./routes/messageRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import connectToMongoDB from "./model/db.js";
 import cookieParser from "cookie-parser";
 
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("hello!");
